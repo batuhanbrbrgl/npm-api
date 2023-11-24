3f0a463b99f33bfc8b3d50949dc0070e98196f67
batuhanbrbrgl <bthnnapcan@gmail.com>
2023-11-24 17:16:21 +0300

    Add download-stats dependency and create stats.get API endpoint

---
+++ server/api/stats.get.ts
@@ -0,0 +1,26 @@
+import stats from "download-stats";
+
+export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
+  
+    async function getStats() {
+      try {
+        const results = await new Promise((resolve, reject) => {
+          stats.get.point("last-day", query.package, function (err, results) {
+            if (err) reject(err);
+            else resolve(results);
+          });
+        });
+  
+        console.log(results);
+        return results;
+      } catch (error) {
+        console.error(error);
+        return "error occurred";
+      }
+    }
+  
+    const result = await getStats();
+    console.log(result);
+    return result;
+  });
